4291986676e00e3d2d64977a7ef714e16f2c14db
Charles <charlestsang0277@gmail.com>
2025-02-12 00:04:22 +0000

    Install packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
-import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
+    test: {
+        environment: 'jsdom',
         alias: {
             '@': path.resolve(__dirname, './src'),
             '@tests': path.resolve(__dirname, './tests')
