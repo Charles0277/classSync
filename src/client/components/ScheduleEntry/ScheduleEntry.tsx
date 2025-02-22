99b107c108c912e7918cf4a9bcb133b05b0b7590
Charles <charlestsang0277@gmail.com>
2025-02-22 18:30:20 +0000

    Adjust styling to fit Icons

--- src/client/components/ScheduleEntry/ScheduleEntry.tsx
+++ src/client/components/ScheduleEntry/ScheduleEntry.tsx
@@ -3,6 +3,9 @@ import {
     IIndividualScheduleEntry
 } from '@/common/types/ISchedule';
 import { convertRoomTypeToClassType } from '@/common/utils';
+import classTypeIcon from '../../assets/classTypeIcon.svg';
+import clockIcon from '../../assets/clockIcon.svg';
+import locationIcon from '../../assets/locationIcon.svg';
 import styles from './ScheduleEntry.module.css';
 
 interface SchedulEntryProps {
@@ -25,11 +28,17 @@ export const ScheduleEntry: React.FC<SchedulEntryProps> = ({
             <div className={styles.entryContent}>
                 <div className={styles.entryTitle}>{entry.className}</div>
                 <div className={styles.entryDetail}>
-                    {convertRoomTypeToClassType(entry.classType)}
-                    <br />
-                    Room: {entry.roomName}
-                    <br />
-                    Time: {entry.hour}:00
+                    <div className={styles.classProperty}>
+                        <img src={classTypeIcon} />{' '}
+                        {convertRoomTypeToClassType(entry.classType)}
+                    </div>
+                    <div className={styles.classProperty}>
+                        <img src={locationIcon} /> Room: {entry.roomName}
+                    </div>
+                    <div className={styles.classProperty}>
+                        <img src={clockIcon} />
+                        Time: {entry.hour}:00
+                    </div>
                 </div>
             </div>
         </div>
