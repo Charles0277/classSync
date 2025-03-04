78e66f8f7ee7b5ae2e1aba55701060a80585b430
Charles <charlestsang0277@gmail.com>
2025-03-04 19:08:59 +0000

    Add and Remove Students from Classes

--- src/client/components/ClassDetails/ClassDetails.tsx
+++ src/client/components/ClassDetails/ClassDetails.tsx
@@ -27,7 +27,7 @@ interface ClassDetailsProps {
 export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
     const { classEntity } = useSelector((state: RootState) => state.class);
     const { token, user } = useSelector((state: RootState) => state.auth);
-    const { users } = useSelector((state: RootState) => state.user);
+    const { users, students } = useSelector((state: RootState) => state.user);
     const { rooms } = useSelector((state: RootState) => state.room);
     const { teachers } = useSelector((state: RootState) => state.user);
 
@@ -40,8 +40,11 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
         classType: entry.classType,
         hour: entry.hour,
         instructorId: (entry as IGlobalScheduleEntry).instructorId,
-        description: classEntity?.description || 'No description provided.'
+        description: classEntity?.description || 'No description provided.',
+        studentIds: (entry as IGlobalScheduleEntry).studentIds
     });
+
+    const isAdmin = user?.role === 'admin';
 
     useEffect(() => {
         if (token && getIdString(classEntity?._id) !== entry.classId) {
@@ -56,7 +59,7 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
     ]);
 
     useEffect(() => {
-        if (classEntity?.students) {
+        if (classEntity?.students?.length) {
             dispatch(
                 fetchUsersRequest({
                     token,
@@ -64,7 +67,7 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
                 })
             );
         }
-    }, [dispatch, classEntity?.students]);
+    }, [dispatch, token, JSON.stringify(classEntity?.students)]);
 
     useEffect(() => {
         setEditedFields((fields) => ({
@@ -80,15 +83,22 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
     }, [dispatch]);
 
     useEffect(() => {
-        if (teachers.length === 0) {
+        if (teachers.length === 0 && isAdmin) {
             dispatch(fetchAllTeachersRequest({ token }));
         }
-    }, [dispatch, token, teachers]);
+    }, [dispatch, token, teachers, isAdmin]);
 
     const teacherOptions = teachers
         ? teachers.map((teacher) => ({
               value: getIdString(teacher._id),
               label: `${teacher.firstName} ${teacher.lastName}`
+          }))
+        : [];
+
+    const studentOptions = students
+        ? students.map((student) => ({
+              value: getIdString(student._id),
+              label: `${student.firstName} ${student.lastName}`
           }))
         : [];
 
@@ -120,10 +130,11 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
     const handleEdit = () => setIsEditing(true);
 
     const handleSave = () => {
-        if (classEntity?._id) {
-            const changedClassFields: Record<string, any> = {};
-            const changedScheduleFields: Record<string, any> = {};
+        if (!classEntity?._id) return;
+        const changedClassFields: Record<string, any> = {};
+        const changedScheduleFields: Record<string, any> = {};
 
+        if (isAdmin) {
             if (editedFields.className !== entry.className) {
                 changedClassFields.name = editedFields.className;
             }
@@ -137,9 +148,6 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
             if (editedFields.classType !== entry.classType) {
                 changedClassFields.classTypes = editedFields.classType;
             }
-            if (editedFields.description !== classEntity.description) {
-                changedClassFields.description = editedFields.description;
-            }
             if (
                 editedFields.roomId !== (entry as IGlobalScheduleEntry).roomId
             ) {
@@ -149,24 +157,41 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
                 changedScheduleFields.hour = editedFields.hour;
             }
 
-            if (Object.keys(changedScheduleFields).length > 0) {
-                dispatch(
-                    updateGlobalScheduleRequest({
-                        token,
-                        id: entry.classId,
-                        formData: changedScheduleFields
-                    })
-                );
+            const sortedEditedStudentIds = [...editedFields.studentIds]
+                .sort()
+                .toString();
+            const sortedEntryStudentIds = [
+                ...(entry as IGlobalScheduleEntry).studentIds
+            ]
+                .sort()
+                .toString();
+            if (sortedEditedStudentIds !== sortedEntryStudentIds) {
+                changedScheduleFields.studentIds = editedFields.studentIds;
+                changedClassFields.students = editedFields.studentIds;
             }
-            if (Object.keys(changedClassFields).length > 0) {
-                dispatch(
-                    updateClassRequest({
-                        token,
-                        id: classEntity._id,
-                        formData: changedClassFields
-                    })
-                );
-            }
+        }
+
+        if (editedFields.description !== classEntity.description) {
+            changedClassFields.description = editedFields.description;
+        }
+
+        if (Object.keys(changedScheduleFields).length > 0) {
+            dispatch(
+                updateGlobalScheduleRequest({
+                    token,
+                    id: entry.classId,
+                    formData: changedScheduleFields
+                })
+            );
+        }
+        if (Object.keys(changedClassFields).length > 0) {
+            dispatch(
+                updateClassRequest({
+                    token,
+                    id: classEntity._id,
+                    formData: changedClassFields
+                })
+            );
         }
         setIsEditing(false);
     };
@@ -178,14 +203,15 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
             classType: entry.classType,
             hour: entry.hour,
             instructorId: entry.instructorName,
-            description: classEntity?.description || 'No description provided.'
+            description: classEntity?.description || 'No description provided.',
+            studentIds: (entry as IGlobalScheduleEntry).studentIds
         });
         setIsEditing(false);
     };
 
     return (
         <div className={styles.classDetails}>
-            {user?.role === 'admin' && !isEditing && (
+            {isAdmin && !isEditing && (
                 <div className={styles.editButton}>
                     <Button
                         type="button"
@@ -198,7 +224,7 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
             )}
             <div className={styles.classProperty}>
                 <span className={styles.title}>Title:</span>
-                {isEditing ? (
+                {isEditing && isAdmin ? (
                     <input
                         className={styles.descriptionInput}
                         type="text"
@@ -216,7 +242,7 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
             </div>
             <div className={styles.classProperty}>
                 <span className={styles.title}>Room:</span>
-                {isEditing ? (
+                {isEditing && isAdmin ? (
                     <Select
                         options={roomOptions}
                         defaultValue={{
@@ -248,7 +274,7 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
             </div>
             <div className={styles.classProperty}>
                 <span className={styles.title}>Class Type:</span>{' '}
-                {isEditing ? (
+                {isEditing && isAdmin ? (
                     <Select
                         options={classTypesOptions}
                         defaultValue={{
@@ -280,7 +306,7 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
             </div>
             <div className={styles.classProperty}>
                 <span className={styles.title}>Time:</span>{' '}
-                {isEditing ? (
+                {isEditing && isAdmin ? (
                     <Select
                         options={timeOptions}
                         defaultValue={{
@@ -312,7 +338,7 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
             </div>
             <div className={styles.classProperty}>
                 <span className={styles.title}>Teacher:</span>{' '}
-                {isEditing ? (
+                {isEditing && isAdmin ? (
                     <Select
                         options={teacherOptions}
                         defaultValue={{
@@ -391,13 +417,53 @@ export const ClassDetails: React.FC<ClassDetailsProps> = ({ entry }) => {
                 )}
             </div>
             {user?.role !== 'student' && (
-                <div className={styles.classProperty}>
+                <div
+                    className={`${styles.classProperty} ${isEditing && isAdmin ? styles.editingStudents : ''}`}
+                >
                     <span className={styles.title}>Enrolled Students:</span>{' '}
-                    <div className={styles.students}>
-                        {users.map(
-                            (user) => `${user.firstName} ${user.lastName} \n`
-                        )}
-                    </div>
+                    {isEditing && isAdmin ? (
+                        <Select
+                            options={studentOptions}
+                            isMulti={true}
+                            defaultValue={students
+                                .filter((student) =>
+                                    editedFields.studentIds.includes(
+                                        getIdString(student._id)
+                                    )
+                                )
+                                .map((student) => ({
+                                    value: getIdString(student._id),
+                                    label: `${student.firstName} ${student.lastName}`
+                                }))}
+                            onChange={(selectedStudents) => {
+                                setEditedFields({
+                                    ...editedFields,
+                                    studentIds: selectedStudents
+                                        ? selectedStudents.map(
+                                              (student) => student.value
+                                          )
+                                        : []
+                                });
+                            }}
+                            placeholder="Select Students"
+                            styles={{
+                                container: (base) => ({
+                                    ...base,
+                                    padding: '0.5rem',
+                                    marginRight: '-0.5rem',
+                                    marginLeft: '-0.5rem'
+                                })
+                            }}
+                            maxMenuHeight={220}
+                        />
+                    ) : (
+                        <div className={styles.students}>
+                            {users.map(
+                                (user) =>
+                                    `${user.firstName} ${user.lastName} \n`
+                            )}
+                        </div>
+                    )}
                 </div>
             )}
         </div>
