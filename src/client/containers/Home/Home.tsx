fb658e551d5f67647e64d0f8bdef32d7902b18fc
Charles <charlestsang0277@gmail.com>
2025-03-08 22:36:30 +0000

    Correcting checking for userSchedule existance

--- src/client/containers/Home/Home.tsx
+++ src/client/containers/Home/Home.tsx
@@ -65,7 +65,7 @@ const Home = () => {
 
     return (
         <PageContainer>
-            {userSchedule ? (
+            {userSchedule?.length ? (
                 <Schedule userSchedule={userSchedule} />
             ) : user?.role === 'admin' ? (
                 globalSchedule?.length ? (
