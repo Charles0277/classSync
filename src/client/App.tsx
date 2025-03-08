83e44441f7c4b0963b305c26cfded17fb9b3fcb3
Charles <charlestsang0277@gmail.com>
2025-03-08 22:35:52 +0000

    Shadcn Toasts Added

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,10 +2,12 @@ import { Provider } from 'react-redux';
 import './App.css';
 import MainContent from './MainContent';
 import store from './store/store';
+import { Toaster } from './components/ui/sonner';
 
 function App() {
     return (
         <Provider store={store}>
+            <Toaster position="bottom-right" />
             <MainContent />
         </Provider>
     );
